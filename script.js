506c47bfc6d1382b621f337037c97eff75a175e1
ShittyAdvice <ShittyAdvice@users.noreply.github.com>
2015-09-17 00:48:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-var parent = document.getElementById("canvasWrapper");
-var canvas = new Canvas("newCanvas", parent, 100, 100);
-
-var line = new Line(canvas, 50, 50, 100, 0);
-var line2 = Line.Copy(line);
-line2.angle += 30;
-line2.Attach(line);
-
-var arc = new Arc(line, line2);
-canvas.StartAnimation();
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+        init();
+    }
+}, 10);
+function init(){
+  var parent = document.getElementById("canvasWrapper");
+  var canvas = new Canvas("newCanvas", parent, 100, 100);
+  
+  var line = new Line(canvas, 50, 50, 100, 0);
+  var line2 = Line.Copy(line);
+  line2.angle += 30;
+  line2.Attach(line);
+  
+  var arc = new Arc(line, line2);
+  canvas.StartAnimation();
+}
