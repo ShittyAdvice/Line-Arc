864f3d6474cc3db753bca0916f58481443ffc288
ShittyAdvice <jetseprive@gmail.com>
2015-09-16 14:31:12 +0200

    Code cleanup

---
+++ math.js
@@ -0,0 +1,24 @@
+/*
+ * x + r * Math.cos(h);
+ * x = X Positie middelpunt
+ * y = Y Positie middelpunt
+ * s = Radius
+ * h = Hoek in Radians
+ */
+var MyMath = {};
+
+MyMath.CircleRingX = function(x,r,a){
+    return x + r * Math.cos(DegreeToRadian(a));
+};
+
+MyMath.CircleRingY = function(y,r,a){
+    return y + r * Math.sin(DegreeToRadian(a));
+};
+
+MyMath.DegreeToRadian = function(degrees){
+    return degrees * (Math.PI / 180);
+};
+
+MyMath.RadianToDegree = function(radian){
+    return radian * (180 / Math.PI);
+};
